a89415a072aebe177bc1a98b6d08e2b228abc903
tamjor <jor22008@byui.edu>
2023-01-04 11:55:59 -0700

    Completed Prep1 and Prep2 programs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,93 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string studentGrade = Console.ReadLine();
+        int studentPercentage = int.Parse(studentGrade);
+        string grade = "";
+        string sign = "";
+
+        if (studentPercentage >= 90)
+        {
+            grade = "A";
+            
+            if (studentPercentage <= 93)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+        }
+
+        else if (studentPercentage >= 80 && studentPercentage < 90)
+        {
+            grade = "B";
+            if (studentPercentage >= 87)
+            {
+                sign = "+";
+            }
+            else if (studentPercentage <= 83)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+        }
+
+        else if (studentPercentage >= 70 && studentPercentage < 80)
+        {
+            grade = "C";
+            if (studentPercentage >= 77)
+            {
+                sign = "+";
+            }
+            else if (studentPercentage <= 73)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+        }
+
+        else if (studentPercentage >= 60 && studentPercentage < 70)
+        {
+            grade = "D";
+            if (studentPercentage >= 67)
+            {
+                sign = "+";
+            }
+            else if (studentPercentage <= 63)
+            {
+                sign = "-";
+            }
+            else
+            {
+                sign = "";
+            }
+        }
+
+        else
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {grade}{sign}.");
+
+        if (studentPercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you have passed the class!");
+        } 
+
+        if (studentPercentage < 70)
+        {
+            Console.WriteLine("Unfortunately, you have not passed the class. Don't give up! Try again.");
+        } 
+
     }
 }
