7c66f0f385806d92d85cb2ca80a4cafac242ef73
tamjor <jor22008@byui.edu>
2023-01-18 13:24:18 -0700

    completed programs for week 03

---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,18 @@
+using System;
+public class Resume
+{
+    public string _name = "";
+    public List<Job> _jobs = new List<Job>();
+
+    public Resume()
+    {
+    }
+
+    public void ShowResume()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        foreach (Job item in _jobs)
+            item.ShowJob();
+    }
+}
